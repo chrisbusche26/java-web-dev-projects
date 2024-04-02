e55b9049134ef5298fcaa6f4b0edd9a24d4bfad5
chrisbusche26 <chrisbusche26@gmail.com>
2024-04-01 20:45:28 -0500

    Unit 2 class 1 studio

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -8,10 +8,24 @@ public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
-        double radius = input.nextDouble();
-        input.close();
-        Double area = Circle.getArea(radius);
+        //String radius = input.nextLine();
+
+        if (input.hasNextDouble()) {
+            double radius = input.nextDouble();
+            if (radius > 0) {
+                input.close();
+                Double area = Circle.getArea(radius);
+                System.out.println("The area of a circle of radius " + radius + " is " + area);
+            } else {
+                System.out.println("Enter positive number.");
+            }
+
+        } else {
+            System.out.println("Enter a number.");
+        }
+
+
         //double area = 3.14 * (radius * radius);
-        System.out.println("The area of a circle of radius " + radius + " is " + area);
+
     }
 }
