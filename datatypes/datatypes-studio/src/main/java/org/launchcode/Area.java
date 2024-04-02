8b6b8f5cb15b85b886ce547212f5fbed5c44e6f9
chrisbusche26 <chrisbusche26@gmail.com>
2024-04-01 19:52:08 -0500

    Unit 2 class 1 studio

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,17 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+
+public class Area {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a radius: ");
+        double radius = input.nextDouble();
+        input.close();
+        Double area = Circle.getArea(radius);
+        //double area = 3.14 * (radius * radius);
+        System.out.println("The area of a circle of radius " + radius + " is " + area);
+    }
+}
